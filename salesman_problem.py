ac6cb82d17efc4c4728d7a2ce41b8f96dc0ea7d9
Michał Świętek <mswietku@gmail.com>
2020-01-09 16:43:19 +0100

    Salesman problem - create fitness function, read cities costs from excel table

--- salesman_problem.py
+++ salesman_problem.py
@@ -1,10 +1,12 @@
 from genetic_algorithm import GeneticAlgorithm
 import random
+import pandas
 
 # Genetic algorithm to solve travelling salesman problem
 # wikipedia link: https://en.wikipedia.org/wiki/Travelling_salesman_problem
 
-NUMBER_OF_CITIES = 7
+cities_costs = pandas.read_excel('salesman_problem.xls', sheet_name='Arkusz1')
+NUMBER_OF_CITIES = len(cities_costs.columns) - 1
 
 
 def create_individual(individual_size):
@@ -30,13 +32,29 @@ def crossover(left_individual, right_individual):
            (new_right_individual_head + left_individual_tail)
 
 
+def get_cost_to_city(current_city_index, next_city_index):
+    current_city_costs = cities_costs[current_city_index].tolist()
+    return current_city_costs[next_city_index]
+
+
+def calculate_fitness(individual):
+    summed_cost = 0
+    for index in range(len(individual)-1):
+        current_city_index = individual[index]
+        next_city_index = individual[index+1]
+        summed_cost += get_cost_to_city(current_city_index, next_city_index)
+
+    return -summed_cost
+
+
 ga = GeneticAlgorithm(individual_function=create_individual,
                       individual_size=NUMBER_OF_CITIES,
                       population_size=100,
                       mutation_function=swap_two_elements,
                       mutation_rate=0.1,
                       max_iterations_number=1000,
-                      fitness_function=sum,
+                      fitness_function=calculate_fitness,
                       crossover_function=crossover)
 
 ga.run_algorithm()
+
